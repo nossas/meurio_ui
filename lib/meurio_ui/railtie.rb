17edc36bafc35151e6c2e0d930ebfce1e5c6ab7f
Nícolas Iensen <nicolas@iensen.me>
2013-11-20 10:17:45 -0200

    Add meurio header sass file

--- lib/meurio_ui/railtie.rb
+++ lib/meurio_ui/railtie.rb
@@ -1,9 +1,11 @@
 require "meurio_ui/view_helpers"
 
 module MeurioUi
-  class Railtie < Rails::Railtie
-    initializer "meurio_ui.view_helpers" do
-      ActionView::Base.send :include, ViewHelpers
+  module Rails
+    class Railtie < ::Rails::Railtie
+      initializer "meurio_ui.view_helpers" do
+        ActionView::Base.send :include, ViewHelpers
+      end
     end
   end
 end
