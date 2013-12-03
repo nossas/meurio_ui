508bfcb095c001d7e23b196c097e09efba6522b4
Nícolas Iensen <nicolas@iensen.me>
2013-12-03 12:01:59 -0200

    Remove header tag

--- app/assets/javascripts/meurio_ui.js
+++ app/assets/javascripts/meurio_ui.js
@@ -1,6 +1,5 @@
 $(function(){
-  $('header.meurio_header .meurio_apps').hover(function(e){ $('.other_apps').toggle(); e.stopPropagation(); return false; });
-
-  $('header.meurio_header .icon-triangle-down').click(function(e){ $('.current_user_links').toggle(); e.stopPropagation(); return false; });
+  $('.meurio_header .meurio_apps').hover(function(e){ $('.other_apps').toggle(); e.stopPropagation(); return false; });
+  $('.meurio_header .icon-triangle-down').click(function(e){ $('.current_user_links').toggle(); e.stopPropagation(); return false; });
   $(document).click(function(){ $('.current_user_links').hide(); });  
 });
