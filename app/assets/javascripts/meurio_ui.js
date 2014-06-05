6bf3514921144a4fa86e5e5068b1a16228ece1bc
Nícolas Iensen <nicolas@iensen.me>
2014-06-05 17:10:44 -0300

    [#71261034] add MC support alert in the header

--- app/assets/javascripts/meurio_ui.js
+++ app/assets/javascripts/meurio_ui.js
@@ -1,5 +1,7 @@
 $(function(){
+  $(document).foundation();
   $('.meurio_header .meurio_apps').hover(function(e){ $('.other_apps').toggle(); e.stopPropagation(); return false; });
   $('.meurio_header .icon-triangle-down').click(function(e){ $('.current_user_links').toggle(); e.stopPropagation(); return false; });
-  $(document).click(function(){ $('.current_user_links').hide(); });  
+  $(document).click(function(){ $('.current_user_links').hide(); });
+  $("a.icon-warning").trigger("click");
 });
