2f713ac28c92c8f3f22330c3241b06dfc5e86a14
Nícolas Iensen <nicolas@iensen.me>
2014-06-09 08:28:38 -0300

    [#71261034] add local storage support for MC warning

--- app/assets/javascripts/meurio_ui.js
+++ app/assets/javascripts/meurio_ui.js
@@ -3,5 +3,10 @@ $(function(){
   $('.meurio_header .meurio_apps').hover(function(e){ $('.other_apps').toggle(); e.stopPropagation(); return false; });
   $('.meurio_header .icon-triangle-down').click(function(e){ $('.current_user_links').toggle(); e.stopPropagation(); return false; });
   $(document).click(function(){ $('.current_user_links').hide(); });
-  $("a.icon-warning").trigger("click");
+
+  // My Cities warning
+  if(localStorage.getItem("myCitiesWarning") == undefined){
+    $("a.icon-warning").trigger("click");
+    localStorage.setItem("myCitiesWarning", true);
+  }
 });
